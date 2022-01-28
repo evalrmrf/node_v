9766b7a8ba8dd2604f0fa949e56abc7b8181fad2
roe <roe@alteos.com>
2022-01-28 17:00:23 +0100

    Test error output

--- bin/node_v.js
+++ bin/node_v.js
@@ -5,6 +5,7 @@ const { exec } = require('child_process');
 exec('sh bin/cli.sh',
     (error, stdout, stderr) => {
         if (stderr){
+            console.log(stderr)
             console.log('Sorry, I am not able to check your NodeJS and NPM versions')
             return true
         }
